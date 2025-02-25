0f4feb5be73442d0c0ad128f60c78de25b509467
Lachlan Van <z5600533@ad.unsw.edu.au>
2025-02-26 00:22:52 +1100

    solutions to backend tasks 1 2 and 3 in typescript, for devsoc subcom technical assessment

--- backend/ts_template/devdonalds.ts
+++ backend/ts_template/devdonalds.ts
@@ -45,24 +45,69 @@ app.post("/parse", (req:Request, res:Response) => {
 // [TASK 1] ====================================================================
 // Takes in a recipeName and returns it in a form that 
 const parse_handwriting = (recipeName: string): string | null => {
-  // TODO: implement me
-  return recipeName
+  recipeName = recipeName.replace(/[-_]/g, ' ');
+  recipeName = recipeName.replace(/\s+/g, ' ').trim();
+  recipeName = recipeName.replace(/[^a-zA-Z]/g, '');
+  recipeName = recipeName.replace(/\b\w/g, char => char.toUpperCase());
+  if (recipeName.length <= 0) {
+    return null;
+  }
+  return recipeName;
 }
 
 // [TASK 2] ====================================================================
 // Endpoint that adds a CookbookEntry to your magical cookbook
-app.post("/entry", (req:Request, res:Response) => {
-  // TODO: implement me
-  res.status(500).send("not yet implemented!")
 
+app.post("/entry", (req:Request, res:Response) => {
+  if (req.body.type !== "recipe" && req.body.type !== "ingredient") {
+    return res.status(400);
+  } else if (req.body.cookTime < 0) {
+    return res.status(400);
+  } 
+  
+  for (const item of cookbook) {
+    if ((req.body.name) === item.name) {
+      return res.status(400);
+    }
+  }
+
+  if (req.body.type === 'recipe') {
+    const seenIngredients: string[] = [];
+    for (const ingredient of req.body.requiredItems) {
+      if (seenIngredients.includes(ingredient.name)) {
+        return res.status(400);
+      } else {
+        seenIngredients.push(ingredient.name);
+      }
+    }
+  }
+  cookbook.push(req.body);
+  res.status(200);
 });
 
 // [TASK 3] ====================================================================
 // Endpoint that returns a summary of a recipe that corresponds to a query name
 app.get("/summary", (req:Request, res:Request) => {
-  // TODO: implement me
-  res.status(500).send("not yet implemented!")
+  const summary: any = null;
+  summary.ingredients = [];
+  
+  if (req.body.type === 'recipe' && cookbook.find((item) => item.name === 
+  req.body.name) === false) {
+    return res.status(400);
+  } 
+  let totalCookTime = 0;
+  for (const item of req.body.requiredItems) {
+    if (cookbook.includes(item.name) === false) {
+      return res.status(400);
+    } else {
+      totalCookTime += item.quantity // * cooktime.;
+      summary.ingredients.push(/*{ name: item.name, quantity: item.quantity}*/);
+    }
+  }
+  summary.name = req.body.name, 
+  summary.cookTime = totalCookTime;
 
+  res.status(200);
 });
 
 // =============================================================================
